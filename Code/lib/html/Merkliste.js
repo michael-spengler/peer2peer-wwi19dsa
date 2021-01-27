06c06bc84ff1cda0078a968bb7a04b607fb3160a
bengoeller <74546142+bengoeller@users.noreply.github.com>
2021-01-27 16:33:42 +0100

    Add files via upload

---
+++ Code/lib/html/Merkliste.js
@@ -0,0 +1,38 @@
+function myFunction() {
+
+    
+   
+   
+    var x = document.getElementById('Card');
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+
+    
+   
+   
+    var x = document.getElementById('Card2');
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction3() {
+
+    
+   
+   
+    var x = document.getElementById('Card3');
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
